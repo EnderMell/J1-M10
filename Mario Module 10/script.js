0b5867c34b14572d553bb9aafa18c07cfde916b0
EnderMell <220813@student.glu.nl>
2023-06-23 08:36:14 +0200

    Comments

--- Mario Module 10/script.js
+++ Mario Module 10/script.js
@@ -4,7 +4,7 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-function init(){
+
 
 const gravity = 0.5
 class Player {
@@ -39,19 +39,21 @@ class Player {
 
 }
 //PLatform Properties
-class Platform { 
-    constructor({x, y}) {
-    this.position ={
-        x: x,
-        y: y,
+class Platform {
+    constructor({ x, y }) {
+        this.position = {
+            x: x,
+            y: y,
+        }
+        
+        this.width = 500
+        this.height = 50
     }
-    this.width = 500
-    this.height = 50
-}
 
 
 
-    draw(){
+
+    draw() {
         c.fillStyle = 'lightblue'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
@@ -60,15 +62,17 @@ class Platform {
 //Platform Placement
 const player = new Player()
 const platforms = [new Platform({
-    x: 0, y: 750
-}), new Platform( {
+    x: 0, y: 750,
+}), new Platform({
     x: 500, y: 750
-}) ]
+})
+
+]
 const keys = {
-    right:{ 
+    right: {
         pressed: false
     },
-    left:{ 
+    left: {
         pressed: false
     }
 }
@@ -80,53 +84,59 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
-    platforms.forEach(platform =>{
+    platforms.forEach(platform => {
         platform.draw()
     })
-
-    if ( keys.right.pressed && player.position.x < 400) {
+        //player moving
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 7
-    } else if (( keys.left.pressed && player.position.x > 100) ||
-    (keys.left.pressed && scrollOffset === 0 && player.position.x > 0)){
+    } else if ((keys.left.pressed && player.position.x > 100) ||
+        (keys.left.pressed && scrollOffset === 0 && player.position.x > 0)) {
         player.velocity.x = -7
     }
-    else { player.velocity.x = 0
-    
-        if ( keys.right.pressed){
+    else {
+        player.velocity.x = 0
+            // Platforms Moving
+        if (keys.right.pressed) {
             scrollOffset += 7
-            platforms.forEach(platform =>{
+            platforms.forEach(platform => {
                 platform.position.x -= 7
             })
-        
-        } else if (keys.left.pressed && scrollOffset > 0){
-            scrollOffset -=7
-            platforms.forEach(platform =>{
+
+        } else if (keys.left.pressed && scrollOffset > 0) {
+            scrollOffset -= 7
+            platforms.forEach(platform => {
                 platform.position.x += 7
             })
-            
-        }    }
+
+        }
+    }
     //platform collision
-    platforms.forEach(platform =>{
-    if (player.position.y + player.height <= platform.position.y &&
-        player.position.y + player.height + player.velocity.y >= platform.position.y &&
-        player.position.x + player.width >= platform.position.x &&
-        player.position.x <= platform.position.x + platform.width){
-        player.velocity.y = 0
-    }
-})
+    platforms.forEach(platform => {
+        if (player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    })
 
-}
- //win condition
-    if ( scrollOffset > 2000) {
-        alert ('You win!')
+
+    //win condition
+    if (scrollOffset > 2000) {
+        alert('You win!')
     }
 
-//lose condition
-    if ( player.position.y > canvas.height) {
-        init()
-    }
+    //lose condition
+    if (player.position.y > canvas.height) {
+        player.position.x = 100;
+        player.position.y = 100;
+
+        }
 }
 animate()
+
+// Controls
 
 window.addEventListener('keydown', ({ keyCode }) => {
     switch (keyCode) {
